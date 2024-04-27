20e5fafc389dfa606cebb6a9bbb8d0e4b597457a
Rahul Singh <rahulsingh.dev.36@gmail.com>
2024-04-27 23:15:54 +0530

    update login/register page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+// const postModel = require("./post");
+var passport = require('passport');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
+
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
+
+router.get('/register', function (req, res, next) {
+  res.render('register');
+});
+
+// router.post('/register', function (req, res, next) {
+//   const data = new userModel({
+
+//   })
+// });
 
 module.exports = router;
